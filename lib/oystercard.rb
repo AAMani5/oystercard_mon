f336d1d65d438efd2ced491442f4ef7f1c1d8d94
Alice Armstrong <alicearmstrong@Alices-MacBook-Pro.local>
2017-02-20 16:39:47 +0000

    Implemented deduction and maximum balance (step 7)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
   attr_accessor :balance
+  MAX_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    self.balance += 10
+    fail "Balance cannot exceed £#{MAX_LIMIT}. Current balance is £#{balance}." if (amount + balance) > MAX_LIMIT
+    self.balance += amount
   end
+
+  def deduct(amount)
+    self.balance -= amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,13 @@ describe Oystercard do
     it "allows a customer to top-up" do
       expect{my_oyster.top_up(10)}.to change{my_oyster.balance}.by 10
     end
+
+    it "raises error when top-would would make balance exceed £90" do
+      expect{my_oyster.top_up(91)}.to raise_error "Balance cannot exceed £#{Oystercard::MAX_LIMIT}. Current balance is £#{my_oyster.balance}."
+    end
+
+    it "deducts fare from a customers' balance" do
+        expect{my_oyster.deduct(10)}.to change{my_oyster.balance}.by -10
+    end
+
 end
