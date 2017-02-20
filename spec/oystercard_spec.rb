6fd0a4aa23a038ad8a4a56b0be1d1039151dcca6
Ashwini <ashie.oh@gmail.com>
2017-02-20 15:57:32 +0000

    One unit test passed(step-5)

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
+    let(:my_oyster) {Oystercard.new}
     it "check to see if card has a balance" do
-    expect(subject.balance).to eq 0
-  end
+    expect(my_oyster.balance).to eq 0
+    end
+
+    it "allows a customer to top-up" do
+      expect(my_oyster).to respond_to :top_up
+    end
 end
